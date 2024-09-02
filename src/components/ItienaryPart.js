9db2c2f707238d4a71a6ecfdd3d6a1639f2bd452
Kamsu Ravi Teeja <94612127+ravirt7911@users.noreply.github.com>
2024-09-02 19:58:05 +0530

    Update ItienaryPart.js

--- src/components/ItienaryPart.js
+++ src/components/ItienaryPart.js
@@ -4,7 +4,7 @@ import PlaceCard from './PlaceCard';
 function ItienaryPart({trip}) {
   return (
     <div className='flex flex-col justify-center items-center'>
-        <h2 className='font-black text-[50px] max-[425px]:text-[35px]'>Your Travel Itienary</h2>
+        <h2 className='font-black text-[50px] max-[425px]:text-[35px]'>Your Travel Itinerary</h2>
         <div className='flex flex-col justify-center items-center'>
             {trip.tripData?.itinerary.map((item,index)=>(
                 <div className='flex flex-col items-center justify-center'>
